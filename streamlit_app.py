aca0a811525e4f955b231a53b15169f7fc8a4b06
tuliosnowflake <140432704+tuliosnowflake@users.noreply.github.com>
2023-07-24 21:07:55 -0400

    Update streamlit_app.py
    added from streamline

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 streamlit.write('Thanks for adding ', add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
